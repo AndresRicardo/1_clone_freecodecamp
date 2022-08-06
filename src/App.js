c196e05a9dbc0813483f5e9a4cd3e9a4050b4bd0
Richi <ricaldo2431@gmail.com>
2022-08-05 21:01:22 -0500

    funciona, con retos implementados, falta resaltar partes del testimonio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import lista from "./Lista_testimonios.js";
+import Testimonio from "./componentes/Testimonio.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    function llenar() {
+        return "lorem ipsum";
+    }
+
+    return (
+        <div className="App">
+            <div className="contenedor-principal">
+                <h1>Here is what our alumn say about freeCodeCamp:</h1>
+                <Testimonio
+                    imagen={lista[0].imagen}
+                    nombre={lista[0].nombre}
+                    pais={lista[0].pais}
+                    cargo={lista[0].cargo}
+                    empresa={lista[0].empresa}
+                    testimonio={lista[0].testimonio}
+                />
+                <Testimonio
+                    imagen={lista[1].imagen}
+                    nombre={lista[1].nombre}
+                    pais={lista[1].pais}
+                    cargo={lista[1].cargo}
+                    empresa={lista[1].empresa}
+                    testimonio={lista[1].testimonio}
+                />
+                <Testimonio
+                    imagen={lista[2].imagen}
+                    nombre={lista[2].nombre}
+                    pais={lista[2].pais}
+                    cargo={lista[2].cargo}
+                    empresa={lista[2].empresa}
+                    testimonio={lista[2].testimonio}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
